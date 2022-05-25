0a73667388333ea890931ebdc632a62b35cbe61b
Mathieu VACANCE <mathieu.vacance@epitech.eu>
2022-05-25 12:18:25 +0200

    Mathieu VACANCE |  fix emmanuel's advices (check mail/password/birthdate & add link to CGU)

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -24,3 +24,19 @@ export const checkEmail = (input: string) => {
   // eslint-disable-next-line no-useless-escape
   return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(input)
 };
+
+export const checkPassword = (input: string) => {
+  // eslint-disable-next-line no-useless-escape
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(input)
+}
+export const checkBirthDate = (input: Date) => {
+    const today = new Date();
+    const dateNaissance = new Date(input);
+
+    let age = today.getFullYear() - dateNaissance.getFullYear();
+    const m = today.getMonth() - dateNaissance.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < dateNaissance.getDate())) {
+        age = age - 1;
+    }
+    return age >= 10;
+}
